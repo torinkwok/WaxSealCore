2e2bfeca011fecaf217a8c3e3c85037f92bb164b
Tong Guo <Tong-G@outlook.com>
2014-12-25 18:12:56 +0800

    Initial Commit

---
+++ WaxSealCore/WaxSealCore.h
@@ -0,0 +1,13 @@
+//
+//  WaxSealCore.h
+//  WaxSealCore
+//
+//  Created by Tong G. on 12/25/14.
+//  Copyright (c) 2014 Tong Guo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WaxSealCore : NSObject
+
+@end
---
+++ WaxSealCore/WaxSealCore.m
@@ -0,0 +1,13 @@
+//
+//  WaxSealCore.m
+//  WaxSealCore
+//
+//  Created by Tong G. on 12/25/14.
+//  Copyright (c) 2014 Tong Guo. All rights reserved.
+//
+
+#import "WaxSealCore.h"
+
+@implementation WaxSealCore
+
+@end
