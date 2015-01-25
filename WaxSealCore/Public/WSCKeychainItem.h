abd1b52276c82f274770f5b3a952cd841716ab6e
Tong Guo <Tong-G@outlook.com>
2015-01-25 16:14:51 +0800

    Updated documentation for `modificationDate` property. #2

--- WaxSealCore/Public/WSCKeychainItem.h
+++ WaxSealCore/Public/WSCKeychainItem.h
@@ -154,7 +154,8 @@ typedef NS_ENUM( FourCharCode, WSCKeychainItemClass )
 
   This is a read-only property, you cannot modify the modification date of an keychain item 
   in *WaxSealCore* framework and *Keychain Services* APIs, as the value you specify will be overwritten with the current time.
-  If you want to change the modification date to something other than the current time, 
+
+  @warning If you want to change the modification date to something other than the current time,
   use a [CSSM](https://developer.apple.com/library/mac/documentation/Security/Conceptual/cryptoservices/CDSA/CDSA.html) function to do so.
   */
 @property ( retain, readonly ) NSDate* modificationDate;
