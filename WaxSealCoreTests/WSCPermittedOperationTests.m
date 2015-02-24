20821d6bce8fbdcb25bfc066eefb4f19a27b9981
Tong Guo <Tong-G@outlook.com>
2015-02-24 23:24:42 +0800

    The properties of `WSCPermittedOperation` have passed their unit tests.

--- WaxSealCoreTests/WSCPermittedOperationTests.m
+++ WaxSealCoreTests/WSCPermittedOperationTests.m
@@ -49,10 +49,12 @@
 @interface WSCPermittedOperationTests : XCTestCase
 
 @property ( retain, readwrite ) WSCPassphraseItem* httpsPassphrase_testCase0;
+@property ( retain, readwrite ) WSCPassphraseItem* applicationsPassphrase_testCase0;
 
 @property ( retain, readwrite ) WSCTrustedApplication* AppleContacts;
 @property ( retain, readwrite ) WSCTrustedApplication* iPhoto;
 @property ( retain, readwrite ) WSCTrustedApplication* Grab;
+@property ( retain, readwrite ) WSCTrustedApplication* myself;
 
 @end
 
@@ -62,22 +64,26 @@
 @implementation WSCPermittedOperationTests
 
 @synthesize httpsPassphrase_testCase0;
+@synthesize applicationsPassphrase_testCase0;
 
 @synthesize AppleContacts;
 @synthesize iPhoto;
+@synthesize Grab;
+@synthesize myself;
 
 - ( void ) setUp
     {
     NSError* error = nil;
+
+    // Construct HTTPS Passphrase
     NSString* serverName_testcase0 = @"secure.imdb.com";
     NSString* relativePath_testCase0 = @"/register-imdb/changepassword/testcase";
-    NSString* accountName_testCase0 = @"Tong-G@outlook.com";
-
+    NSString* httpsAccountName_testCase0 = @"Tong-G@outlook.com";
     self.httpsPassphrase_testCase0 =
         ( WSCPassphraseItem* )[ [ WSCKeychain login ]
             findFirstKeychainItemSatisfyingSearchCriteria: @{ WSCKeychainItemAttributeHostName : serverName_testcase0
                                                             , WSCKeychainItemAttributeRelativePath : relativePath_testCase0
-                                                            , WSCKeychainItemAttributeAccount : accountName_testCase0
+                                                            , WSCKeychainItemAttributeAccount : httpsAccountName_testCase0
                                                             }
                                                 itemClass: WSCKeychainItemClassInternetPassphraseItem
                                                     error: &error ];
@@ -85,7 +91,7 @@
         self.httpsPassphrase_testCase0 =
             [ [ WSCKeychain login ] addInternetPassphraseWithServerName: serverName_testcase0
                                                         URLRelativePath: relativePath_testCase0
-                                                            accountName: accountName_testCase0
+                                                            accountName: httpsAccountName_testCase0
                                                                protocol: WSCInternetProtocolTypeHTTPS
                                                              passphrase: @"waxsealcore"
                                                                   error: &error ];
@@ -93,24 +99,50 @@
     XCTAssertNil( error );
     _WSCPrintNSErrorForUnitTest( error );
 
+    // Construct Application Passphrase
+    NSString* appName_testCase0 = @"WaxSealCore Unit Test";
+    NSString* appAccountName_testCase0 = @"NSTongG";
+    self.applicationsPassphrase_testCase0 =
+        ( WSCPassphraseItem* )[ [ WSCKeychain login ]
+            findFirstKeychainItemSatisfyingSearchCriteria: @{ WSCKeychainItemAttributeServiceName : appName_testCase0
+                                                            , WSCKeychainItemAttributeAccount : appAccountName_testCase0
+                                                            }
+                                                itemClass: WSCKeychainItemClassApplicationPassphraseItem
+                                                    error: &error ];
+    if ( !self.applicationsPassphrase_testCase0 )
+        self.applicationsPassphrase_testCase0 =
+            [ [ WSCKeychain login ] addApplicationPassphraseWithServiceName: appName_testCase0
+                                                                accountName: appAccountName_testCase0
+                                                                 passphrase: @"waxsealcore"
+                                                                      error: &error ];
+    XCTAssertNotNil( self.applicationsPassphrase_testCase0 );
+    XCTAssertNil( error );
+    _WSCPrintNSErrorForUnitTest( error );
+
     self.AppleContacts =
         [ WSCTrustedApplication trustedApplicationWithContentsOfURL: [ NSURL URLWithString: @"/Applications/Contacts.app" ]
-                                                              error: &error ];
-
+                                                              error: nil ];
     self.iPhoto =
         [ WSCTrustedApplication trustedApplicationWithContentsOfURL: [ NSURL URLWithString: @"/Applications/iPhoto.app" ]
-                                                              error: &error ];
+                                                              error: nil ];
     self.Grab =
         [ WSCTrustedApplication trustedApplicationWithContentsOfURL: [ NSURL URLWithString: @"/Applications/Utilities/Grab.app" ]
-                                                              error: &error ];
+                                                              error: nil ];
+    self.myself =
+        [ WSCTrustedApplication trustedApplicationWithContentsOfURL: nil error: nil ];
     }
 
 - ( void ) tearDown
     {
     [ self.httpsPassphrase_testCase0 release ];
+
+    [ self.AppleContacts release ];
+    [ self.iPhoto release ];
+    [ self.Grab release ];
+    [ self.myself release ];
     }
 
-- ( void ) testOperationsProperty
+- ( void ) testProperties
     {
     NSError* error = nil;
 
@@ -119,62 +151,75 @@
     CFArrayRef debugTrustedApps = NULL;
     CFStringRef debugDescriptor = NULL;
     SecKeychainPromptSelector debugPromptSel = 0;
+
     SecKeychainItemCopyAccess( self.httpsPassphrase_testCase0.secKeychainItem, &debugAccess );
-    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 400 +++++++++ +++++++++ +++++++++\n" );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 400 HTTPS +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( debugAccess );
 
-    NSArray* commonPermittedOperations = nil;
-    commonPermittedOperations = [ self.httpsPassphrase_testCase0 permittedOperations ];
+    SecKeychainItemCopyAccess( self.applicationsPassphrase_testCase0.secKeychainItem, &debugAccess );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 400 App +++++++++ +++++++++ +++++++++\n" );
+    _WSCPrintAccess( debugAccess );
+
+    NSArray* commonPermittedOperationsOfHTTPSPassphrase_testCase0 = [ self.httpsPassphrase_testCase0 permittedOperations ];
+    NSArray* commonPermittedOperationsOfAppPassphrase_testCase0 = [ self.applicationsPassphrase_testCase0 permittedOperations ];
 
     // ----------------------------------------------------------------------------------
     // Test Case 0
     // ----------------------------------------------------------------------------------
-    WSCTrustedApplication* trustedApplication_testCase0 = [ WSCTrustedApplication trustedApplicationWithContentsOfURL: nil error: &error ];
-    XCTAssertNil( error );
-    _WSCPrintNSErrorForUnitTest( error );
-
     WSCPermittedOperation* restrictedOperation_testCase0 = nil;
-    for ( WSCPermittedOperation* _PermittedOperation in commonPermittedOperations )
+    for ( WSCPermittedOperation* _PermittedOperation in commonPermittedOperationsOfAppPassphrase_testCase0 )
         {
-        if ( [ _PermittedOperation.trustedApplications containsObject: trustedApplication_testCase0 ] )
+        if ( [ _PermittedOperation.trustedApplications containsObject: self.myself ] )
             {
             restrictedOperation_testCase0 = _PermittedOperation;
             break;
             }
         }
 
-    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithObjects: trustedApplication_testCase0, self.iPhoto, nil ];
-    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 2 );
-
-    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithObjects: trustedApplication_testCase0, self.iPhoto, self.Grab, nil ];
-    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 3 );
-
-    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithArray: @[ trustedApplication_testCase0, self.iPhoto, self.Grab, self.AppleContacts ] ];
-    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 4 );
-
-    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithArray: @[ trustedApplication_testCase0, self.iPhoto, self.Grab, self.AppleContacts ] ];
-    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 4 );
-
     NSLog( @"Passphrase: %@", [ [ [ NSString alloc ] initWithData: self.httpsPassphrase_testCase0.passphrase encoding: NSUTF8StringEncoding ] autorelease ] );
     restrictedOperation_testCase0.promptContext =
-        WSCPermittedOperationPromptContextRequirePassphraseEveryAccess | WSCPermittedOperationPromptContextWhenUnsigned | WSCPermittedOperationPromptContextWhenUnsignedAct
-            | WSCPermittedOperationPromptContextInvalidSigned | WSCPermittedOperationPromptContextInvalidSignedAct;
+        WSCPermittedOperationPromptContextRequirePassphraseEveryAccess | WSCPermittedOperationPromptContextWhenUnsigned;
 
+    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithObjects: self.myself, self.iPhoto, nil ];
+    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 2 );
+
+    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithObjects: self.myself, self.iPhoto, self.Grab, nil ];
+    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 3 );
+
+    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithArray: @[ self.myself, self.iPhoto, self.Grab, self.AppleContacts ] ];
+    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 4 );
+
+    restrictedOperation_testCase0.trustedApplications = [ NSSet setWithArray: @[ self.myself, self.iPhoto, self.Grab, self.AppleContacts ] ];
+    XCTAssertEqual( restrictedOperation_testCase0.trustedApplications.count, 4 );
     NSLog( @"Passphrase: %@", [ [ [ NSString alloc ] initWithData: self.httpsPassphrase_testCase0.passphrase encoding: NSUTF8StringEncoding ] autorelease ] );
+
+    SecKeychainItemCopyAccess( self.applicationsPassphrase_testCase0.secKeychainItem, &debugAccess );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 401 App +++++++++ +++++++++ +++++++++\n" );
+    _WSCPrintAccess( debugAccess );
+
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 402 App +++++++++ +++++++++ +++++++++\n" );
+    _WSCPrintAccess( restrictedOperation_testCase0.hostProtectedKeychainItem.secAccess );
+
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 403 App +++++++++ +++++++++ +++++++++\n" );
+    _WSCPrintAccess( self.applicationsPassphrase_testCase0.secAccess );
+
+    XCTAssertEqual( self.applicationsPassphrase_testCase0, restrictedOperation_testCase0.hostProtectedKeychainItem );
+    XCTAssertEqual( self.applicationsPassphrase_testCase0.hash, restrictedOperation_testCase0.hostProtectedKeychainItem.hash );
+    XCTAssertEqualObjects( self.applicationsPassphrase_testCase0, restrictedOperation_testCase0.hostProtectedKeychainItem );
 
     // ----------------------------------------------------------------------------------
     // Test Case 2
     // ----------------------------------------------------------------------------------
     int count = 0;
-    for ( WSCPermittedOperation* _PermittedOperation in commonPermittedOperations )
+    for ( WSCPermittedOperation* _PermittedOperation in commonPermittedOperationsOfHTTPSPassphrase_testCase0 )
         if ( _PermittedOperation.trustedApplications.count < 3 )
             _PermittedOperation.descriptor = [ NSString stringWithFormat: @"Initial Permitted Operation %d", count++ ];
 
-    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 0 +++++++++ +++++++++ +++++++++\n" );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 400 HTTPS +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( self.httpsPassphrase_testCase0.secAccess );
 
     SecKeychainItemCopyAccess( self.httpsPassphrase_testCase0.secKeychainItem, &debugAccess );
-    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 401 +++++++++ +++++++++ +++++++++\n" );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 401 HTTPS +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( debugAccess );
 
     WSCPermittedOperation* restrictedOperation_testCase2 =
@@ -188,10 +233,10 @@
     _WSCPrintNSErrorForUnitTest( error );
 
     SecKeychainItemCopyAccess( self.httpsPassphrase_testCase0.secKeychainItem, &debugAccess );
-    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 402 +++++++++ +++++++++ +++++++++\n" );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 402 HTTPS +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( debugAccess );
 
-    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 1 +++++++++ +++++++++ +++++++++\n" );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 403 HTTPS +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( self.httpsPassphrase_testCase0.secAccess );
 
     restrictedOperation_testCase2.promptContext =
@@ -208,7 +253,7 @@
     WSCPermittedOperationTag olderTag_testCase2 = restrictedOperation_testCase2.operations;
 
     SecKeychainItemCopyAccess( self.httpsPassphrase_testCase0.secKeychainItem, &debugAccess );
-    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 403 +++++++++ +++++++++ +++++++++\n" );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 404 HTTPS +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( debugAccess );
 
     fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ Before Modifying - Test Case 2 - HTTPS Passphrase Item #0 +++++++++ +++++++++ +++++++++\n" );
@@ -227,7 +272,7 @@
     olderTag_testCase2 = restrictedOperation_testCase2.operations;
 
     SecKeychainItemCopyAccess( self.httpsPassphrase_testCase0.secKeychainItem, &debugAccess );
-    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 404 +++++++++ +++++++++ +++++++++\n" );
+    fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 405 HTTPS +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( debugAccess );
 
     fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ After Modifying - Test Case 2 - HTTPS Passphrase Item #1 +++++++++ +++++++++ +++++++++\n" );
@@ -285,18 +330,6 @@
     SecKeychainItemCopyAccess( self.httpsPassphrase_testCase0.secKeychainItem, &debugAccess );
     fprintf( stdout, "\n+++++++++ +++++++++ +++++++++ +++++++++ DEBUG 303 +++++++++ +++++++++ +++++++++\n" );
     _WSCPrintAccess( debugAccess );
-    }
-
-- ( void ) testPromptContextProperty
-    {
-    // ----------------------------------------------------------------------------------
-    // Test Case 0
-    // ----------------------------------------------------------------------------------
-//    NSArray permittedOperations_testCase0 = nil;
-//    NSArray* permittedOperations_testCase0 = nil;
-//    permittedOperations_testCase0 = [ self.httpsPassphrase_testCase0 permittedOperations ];
-//    for ( WSCPermittedOperation* _PermittedOperation in permittedOperations_testCase0 )
-//        _PermittedOperation.promptContext = WSCPermittedOperationPromptContextRequirePassphraseEveryAccess;
     }
 
 - ( void ) testDescriptorProperty
