02b5c54e155d2c1e9aeab60c369079e83c460e72
Tong Guo <Tong-G@outlook.com>
2015-03-14 00:46:56 +0800

    Backup

--- WaxSealCore/Public/WSCKey.h
+++ WaxSealCore/Public/WSCKey.h
@@ -25,117 +25,201 @@
 #import <Foundation/Foundation.h>
 #import <Security/Security.h>
 
-typedef NS_ENUM( CSSM_ALGORITHMS, WSCKeyAlgorithm )
+/** Defines constants that specify algorithm that was used that key.
+  */
+typedef NS_ENUM( CSSM_ALGORITHMS, WSCAlgorithmType )
     {
-	CSSM_ALGID_NONE
-	CSSM_ALGID_CUSTOM
-	CSSM_ALGID_DH
-	CSSM_ALGID_PH
-	CSSM_ALGID_KEA
-	CSSM_ALGID_MD2
-	CSSM_ALGID_MD4
-	CSSM_ALGID_MD5
-	CSSM_ALGID_SHA1
-	CSSM_ALGID_NHASH
-	CSSM_ALGID_HAVAL
-	CSSM_ALGID_RIPEMD
-	CSSM_ALGID_IBCHASH
-	CSSM_ALGID_RIPEMAC
-	CSSM_ALGID_DES
-	CSSM_ALGID_DESX
-	CSSM_ALGID_RDES
-	CSSM_ALGID_3DES_3KEY_EDE
-	CSSM_ALGID_3DES_2KEY_EDE
-	CSSM_ALGID_3DES_1KEY_EEE
-	CSSM_ALGID_3DES_3KEY
-	CSSM_ALGID_3DES_3KEY_EEE
-	CSSM_ALGID_3DES_2KEY
-	CSSM_ALGID_3DES_2KEY_EEE
-	CSSM_ALGID_3DES_1KEY
-	CSSM_ALGID_IDEA
-	CSSM_ALGID_RC2
-	CSSM_ALGID_RC5
-	CSSM_ALGID_RC4
-	CSSM_ALGID_SEAL
-	CSSM_ALGID_CAST
-	CSSM_ALGID_BLOWFISH
-	CSSM_ALGID_SKIPJACK
-	CSSM_ALGID_LUCIFER
-	CSSM_ALGID_MADRYGA
-	CSSM_ALGID_FEAL
-	CSSM_ALGID_REDOC
-	CSSM_ALGID_REDOC3
-	CSSM_ALGID_LOKI
-	CSSM_ALGID_KHUFU
-	CSSM_ALGID_KHAFRE
-	CSSM_ALGID_MMB
-	CSSM_ALGID_GOST
-	CSSM_ALGID_SAFER
-	CSSM_ALGID_CRAB
-	CSSM_ALGID_RSA
-	CSSM_ALGID_DSA
-	CSSM_ALGID_MD5WithRSA
-	CSSM_ALGID_MD2WithRSA
-	CSSM_ALGID_ElGamal
-	CSSM_ALGID_MD2Random
-	CSSM_ALGID_MD5Random
-	CSSM_ALGID_SHARandom
-	CSSM_ALGID_DESRandom
-	CSSM_ALGID_SHA1WithRSA
-	CSSM_ALGID_CDMF
-	CSSM_ALGID_CAST3
-	CSSM_ALGID_CAST5
-	CSSM_ALGID_GenericSecret
-	CSSM_ALGID_ConcatBaseAndKey
-	CSSM_ALGID_ConcatKeyAndBase
-	CSSM_ALGID_ConcatBaseAndData
-	CSSM_ALGID_ConcatDataAndBase
-	CSSM_ALGID_XORBaseAndData
-	CSSM_ALGID_ExtractFromKey
-	CSSM_ALGID_SSL3PreMasterGen
-	CSSM_ALGID_SSL3MasterDerive
-	CSSM_ALGID_SSL3KeyAndMacDerive
-	CSSM_ALGID_SSL3MD5_MAC
-	CSSM_ALGID_SSL3SHA1_MAC
-	CSSM_ALGID_PKCS5_PBKDF1_MD5
-	CSSM_ALGID_PKCS5_PBKDF1_MD2
-	CSSM_ALGID_PKCS5_PBKDF1_SHA1
-	CSSM_ALGID_WrapLynks
-	CSSM_ALGID_WrapSET_OAEP
-	CSSM_ALGID_BATON =					CSSM_ALGID_NONE + 72,
-	CSSM_ALGID_ECDSA =					CSSM_ALGID_NONE + 73,
-	CSSM_ALGID_MAYFLY =					CSSM_ALGID_NONE + 74,
-	CSSM_ALGID_JUNIPER =				CSSM_ALGID_NONE + 75,
-	CSSM_ALGID_FASTHASH =				CSSM_ALGID_NONE + 76,
-	CSSM_ALGID_3DES =					CSSM_ALGID_NONE + 77,
-	CSSM_ALGID_SSL3MD5 =				CSSM_ALGID_NONE + 78,
-	CSSM_ALGID_SSL3SHA1 =				CSSM_ALGID_NONE + 79,
-	CSSM_ALGID_FortezzaTimestamp =		CSSM_ALGID_NONE + 80,
-	CSSM_ALGID_SHA1WithDSA =			CSSM_ALGID_NONE + 81,
-	CSSM_ALGID_SHA1WithECDSA =			CSSM_ALGID_NONE + 82,
-	CSSM_ALGID_DSA_BSAFE =				CSSM_ALGID_NONE + 83,
-	CSSM_ALGID_ECDH =					CSSM_ALGID_NONE + 84,
-	CSSM_ALGID_ECMQV =					CSSM_ALGID_NONE + 85,
-	CSSM_ALGID_PKCS12_SHA1_PBE =		CSSM_ALGID_NONE + 86,
-	CSSM_ALGID_ECNRA =					CSSM_ALGID_NONE + 87,
-	CSSM_ALGID_SHA1WithECNRA =			CSSM_ALGID_NONE + 88,
-	CSSM_ALGID_ECES =					CSSM_ALGID_NONE + 89,
-	CSSM_ALGID_ECAES =					CSSM_ALGID_NONE + 90,
-	CSSM_ALGID_SHA1HMAC =				CSSM_ALGID_NONE + 91,
-	CSSM_ALGID_FIPS186Random =			CSSM_ALGID_NONE + 92,
-	CSSM_ALGID_ECC =					CSSM_ALGID_NONE + 93,
-	CSSM_ALGID_MQV =					CSSM_ALGID_NONE + 94,
-	CSSM_ALGID_NRA =					CSSM_ALGID_NONE + 95,
-	CSSM_ALGID_IntelPlatformRandom =	CSSM_ALGID_NONE + 96,
-	CSSM_ALGID_UTC =					CSSM_ALGID_NONE + 97,
-	CSSM_ALGID_HAVAL3 =					CSSM_ALGID_NONE + 98,
-	CSSM_ALGID_HAVAL4 =					CSSM_ALGID_NONE + 99,
-	CSSM_ALGID_HAVAL5 =					CSSM_ALGID_NONE + 100,
-	CSSM_ALGID_TIGER =					CSSM_ALGID_NONE + 101,
-	CSSM_ALGID_MD5HMAC =				CSSM_ALGID_NONE + 102,
-	CSSM_ALGID_PKCS5_PBKDF2 = 			CSSM_ALGID_NONE + 103,
-	CSSM_ALGID_RUNNING_COUNTER =		CSSM_ALGID_NONE + 104,
-	CSSM_ALGID_LAST =					CSSM_ALGID_NONE + 0x7FFFFFFF,
+    /// No Algorithm.
+      WSCKeyAlgorithmNone           	= CSSM_ALGID_NONE
+
+    /// Custom Algorithm.
+	, WSCKeyAlgorithmCustom         	= CSSM_ALGID_CUSTOM
+
+	/// Diffie–Hellman key exchange algorithm.
+	, WSCKeyAlgorithmDH             	= CSSM_ALGID_DH
+
+	/// Pohlig-Hellman key exchange algorithm.
+	, WSCKeyAlgorithmPH             	= CSSM_ALGID_PH
+
+	/// Key Exchange algorithm.
+	, WSCKeyAlgorithmKEA            	= CSSM_ALGID_KEA
+
+	/// MD2hash algorithm.
+	, WSCKeyAlgorithmMD2       	        = CSSM_ALGID_MD2
+
+	/// MD4hash algorithm.
+	, WSCKeyAlgorithmMD4            	= CSSM_ALGID_MD4
+
+	/// MD5hash algorithm.
+	, WSCKeyAlgorithmMD5            	= CSSM_ALGID_MD5
+
+	/// Secure Hash algorithm.
+	, WSCKeyAlgorithmSHA1           	= CSSM_ALGID_SHA1
+
+	/// N-Hash algorithm.
+	, WSCKeyAlgorithmNHASH          	= CSSM_ALGID_NHASH
+
+	/// HAVAL hash algorithm (MD5 variant).
+	, WSCKeyAlgorithmHAVAL          	= CSSM_ALGID_HAVAL
+
+	/// RIPE-MD hash algorithm (MD4 variant - developed for the European Community's RIPE project).
+	, WSCKeyAlgorithmRIPEMD         	= CSSM_ALGID_RIPEMD
+
+	/// IBC-Hash (keyed hash algorithm or MAC).
+	, WSCKeyAlgorithmIBCHASH			= CSSM_ALGID_IBCHASH
+
+	/// RIPE-MAC.
+	, WSCKeyAlgorithmRIPEMAC			= CSSM_ALGID_RIPEMAC
+
+	/// Data Encryption Standard block cipher.
+	, WSCKeyAlgorithmDES				= CSSM_ALGID_DES
+
+	/// DESX block cipher (DES variant from RSA).
+	, WSCKeyAlgorithmDESX				= CSSM_ALGID_DESX
+
+	/// RDES block cipher (DES variant).
+	, WSCKeyAlgorithmRDES				= CSSM_ALGID_RDES
+
+	/// Triple-DES block cipher (with 3 keys).
+	, WSCKeyAlgorithm3DESWith3Keys			= CSSM_ALGID_3DES_3KEY
+
+	/// Triple-DES block cipher (with 2 keys).
+	, WSCKeyAlgorithm3DESWith2Keys	    	= CSSM_ALGID_3DES_2KEY
+
+	/// Triple-DES block cipher (with 1 key) Lucifer block cipher.
+	, WSCKeyAlgorithm3DESWith1Key	    	= CSSM_ALGID_3DES_1KEY
+
+	/// International Data Encryption Algorithm (IDEA) block cipher.
+	, WSCKeyAlgorithmIDEA		    	= CSSM_ALGID_IDEA
+
+	/// RC2 block cipher.
+	, WSCKeyAlgorithmRC2  				= CSSM_ALGID_RC2
+
+	/// RC5 block cipher.
+	, WSCKeyAlgorithmRC5		    	= CSSM_ALGID_RC5
+
+	/// RC4 stream cipher.
+	, WSCKeyAlgorithmRC4  				= CSSM_ALGID_RC4
+
+	/// SEAL stream cipher.
+	, WSCKeyAlgorithmSEAL  				= CSSM_ALGID_SEAL
+
+	/// CAST block cipher.
+	, WSCKeyAlgorithmCAST  				= CSSM_ALGID_CAST
+
+	/// Blowfish block cipher.
+	, WSCKeyAlgorithmBlowfish  			= CSSM_ALGID_BLOWFISH
+
+	/// Skipjack block cipher.
+	, WSCKeyAlgorithmSkipjack  			= CSSM_ALGID_SKIPJACK
+
+	/// Lucifer block cipher.
+	, WSCKeyAlgorithmLucifer  			= CSSM_ALGID_LUCIFER
+
+	/// Madryga block cipher.
+	, WSCKeyAlgorithmMadryga  			= CSSM_ALGID_MADRYGA
+
+	/// FEAL (the Fast data Encipherment ALgorithm) block cipher.
+	, WSCKeyAlgorithmFEAL  				= CSSM_ALGID_FEAL
+
+	/// REDOC II block cipher.
+	, WSCKeyAlgorithmREDOC  			= CSSM_ALGID_REDOC
+
+	/// REDOC III block cipher.
+	, WSCKeyAlgorithmREDOC3  			= CSSM_ALGID_REDOC3
+
+	/// LOKI block cipher.
+	, WSCKeyAlgorithmLOKI  				= CSSM_ALGID_LOKI
+
+	/// Khufu block cipher.
+	, WSCKeyAlgorithmKhufu  			= CSSM_ALGID_KHUFU
+
+	/// Khafre block cipher.
+	, WSCKeyAlgorithmKhafre  			= CSSM_ALGID_KHAFRE
+
+	/// MMB block cipher (IDEA variant).
+	, WSCKeyAlgorithmMMB  				= CSSM_ALGID_MMB
+
+	/// GOST block cipher.
+	, WSCKeyAlgorithmGOST  				= CSSM_ALGID_GOST
+
+	/// SAFER (Secure And Fast Encryption Routine) K-40, K-64, K-128 block cipher
+	, WSCKeyAlgorithmSAFER  			= CSSM_ALGID_SAFER
+
+	/// CRAB block cipher.
+	, WSCKeyAlgorithmCRAB  				= CSSM_ALGID_CRAB
+
+	/// RSA public key cipher.
+	, WSCKeyAlgorithmRSA  				= CSSM_ALGID_RSA
+
+	/// DSA (Digital Signature algorithm).
+	, WSCKeyAlgorithmDSA  				= CSSM_ALGID_DSA
+
+	/// MD5/RSA signature algorithm.
+	, WSCKeyAlgorithmMD5WithRSA  		= CSSM_ALGID_MD5WithRSA
+
+	/// MD2/RSA signature algorithm
+	, WSCKeyAlgorithmMD2WithRSA  		= CSSM_ALGID_MD2WithRSA
+	, WSCKeyAlgorithmElGamal  			= CSSM_ALGID_ElGamal
+	, WSCKeyAlgorithmMD2Random 		    = CSSM_ALGID_MD2Random
+	, WSCKeyAlgorithmMD5Random  		= CSSM_ALGID_MD5Random
+	, WSCKeyAlgorithmSHARandom  		= CSSM_ALGID_SHARandom
+	, WSCKeyAlgorithmDESRandom  		= CSSM_ALGID_DESRandom
+	, WSCKeyAlgorithmSHA1WithRSA  		= CSSM_ALGID_SHA1WithRSA
+	, WSCKeyAlgorithmCDMF  				= CSSM_ALGID_CDMF
+	, WSCKeyAlgorithmCAST3  			= CSSM_ALGID_CAST3
+	, WSCKeyAlgorithmCAST5  			= CSSM_ALGID_CAST5
+	, WSCKeyAlgorithmGenericSecret  	= CSSM_ALGID_GenericSecret
+	, WSCKeyAlgorithmConcatBaseAndKey 	= CSSM_ALGID_ConcatBaseAndKey
+	, WSCKeyAlgorithmConcatKeyAndBase  	= CSSM_ALGID_ConcatKeyAndBase
+	, WSCKeyAlgorithmConcatBaseAndData  = CSSM_ALGID_ConcatBaseAndData
+	, WSCKeyAlgorithmConcatDataAndBase  = CSSM_ALGID_ConcatDataAndBase
+	, WSCKeyAlgorithmXORBaseAndData  	= CSSM_ALGID_XORBaseAndData
+	, WSCKeyAlgorithmExtractFromKey  	= CSSM_ALGID_ExtractFromKey
+	, WSCKeyAlgorithmSSL3PreMasterGen   = CSSM_ALGID_SSL3PreMasterGen
+	, WSCKeyAlgorithmSSL3MasterDerive  	= CSSM_ALGID_SSL3MasterDerive
+	, WSCKeyAlgorithmSSL3KeyAndMacDerive= CSSM_ALGID_SSL3KeyAndMacDerive
+	, WSCKeyAlgorithmSSL3MD5_MAC  		= CSSM_ALGID_SSL3MD5_MAC
+	, WSCKeyAlgorithmSSL3SHA1_MAC  		= CSSM_ALGID_SSL3SHA1_MAC
+	, WSCKeyAlgorithmPKCS5_PBKDF1_MD5  	= CSSM_ALGID_PKCS5_PBKDF1_MD5
+	, WSCKeyAlgorithmPKCS5_PBKDF1_MD2  	= CSSM_ALGID_PKCS5_PBKDF1_MD2
+	, WSCKeyAlgorithmPKCS5_PBKDF1_SHA1  = CSSM_ALGID_PKCS5_PBKDF1_SHA1
+	, WSCKeyAlgorithmWrapLynks  		= CSSM_ALGID_WrapLynks
+	, WSCKeyAlgorithmWrapSET_OAEP  		= CSSM_ALGID_WrapSET_OAEP
+	, WSCKeyAlgorithmBATON  			= CSSM_ALGID_BATON
+	, WSCKeyAlgorithmECDSA  			= CSSM_ALGID_ECDSA
+	, WSCKeyAlgorithmMAYFLY  			= CSSM_ALGID_MAYFLY
+	, WSCKeyAlgorithmJUNIPER 			= CSSM_ALGID_JUNIPER
+	, WSCKeyAlgorithmFASTHASH  			= CSSM_ALGID_FASTHASH
+	, WSCKeyAlgorithm3DES  				= CSSM_ALGID_3DES
+	, WSCKeyAlgorithmSSL3MD5  			= CSSM_ALGID_SSL3MD5
+	, WSCKeyAlgorithmSSL3SHA1  			= CSSM_ALGID_SSL3SHA1
+	, WSCKeyAlgorithmFortezzaTimestamp  = CSSM_ALGID_FortezzaTimestamp
+	, WSCKeyAlgorithmSHA1WithDSA  		= CSSM_ALGID_SHA1WithDSA
+	, WSCKeyAlgorithmSHA1WithECDSA  	= CSSM_ALGID_SHA1WithECDSA
+	, WSCKeyAlgorithmDSA_BSAFE  		= CSSM_ALGID_DSA_BSAFE
+	, WSCKeyAlgorithmECDH  				= CSSM_ALGID_ECDH
+	, WSCKeyAlgorithmECMQV  			= CSSM_ALGID_ECMQV
+	, WSCKeyAlgorithmPKCS12_SHA1_PBE  	= CSSM_ALGID_PKCS12_SHA1_PBE
+	, WSCKeyAlgorithmECNRA  			= CSSM_ALGID_ECNRA
+	, WSCKeyAlgorithmSHA1WithECNRA  	= CSSM_ALGID_SHA1WithECNRA
+	, WSCKeyAlgorithmECES  				= CSSM_ALGID_ECES
+	, WSCKeyAlgorithmECAES  			= CSSM_ALGID_ECAES
+	, WSCKeyAlgorithmSHA1HMAC  			= CSSM_ALGID_SHA1HMAC
+	, WSCKeyAlgorithmFIPS186Random  	= CSSM_ALGID_FIPS186Random
+	, WSCKeyAlgorithmECC  				= CSSM_ALGID_ECC
+	, WSCKeyAlgorithmMQV  				= CSSM_ALGID_MQV
+	, WSCKeyAlgorithmNRA 	 			= CSSM_ALGID_NRA
+	, WSCKeyAlgorithmIntelPlatformRandom= CSSM_ALGID_IntelPlatformRandom
+	, WSCKeyAlgorithmUTC  				= CSSM_ALGID_UTC
+	, WSCKeyAlgorithmHAVAL3  			= CSSM_ALGID_HAVAL3
+	, WSCKeyAlgorithmHAVAL4  			= CSSM_ALGID_HAVAL4
+	, WSCKeyAlgorithmHAVAL5  			= CSSM_ALGID_HAVAL5
+	, WSCKeyAlgorithmTIGER  			= CSSM_ALGID_TIGER
+	, WSCKeyAlgorithmMD5HMAC  			= CSSM_ALGID_MD5HMAC
+	, WSCKeyAlgorithmPKCS5_PBKDF2  		= CSSM_ALGID_PKCS5_PBKDF2
+	, WSCKeyAlgorithmRUNNING_COUNTER  	= CSSM_ALGID_RUNNING_COUNTER
+	, WSCKeyAlgorithmLAST  				= CSSM_ALGID_LAST
     };
 
 /** The `WSCKey` class is a subclass of `WSCKeychainItem` representing a key that is stored in a keychain.
